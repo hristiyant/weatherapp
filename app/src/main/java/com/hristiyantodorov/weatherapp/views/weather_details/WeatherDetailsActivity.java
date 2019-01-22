3c30968cf6a9b2fa9e950df54574f85cf0c6f534
Hrisko <hristiyan.todorov@adastragrp.com>
2019-01-22 17:59:01 +0200

    Implement Weather Details screen - add activity, fragment and corresponding layouts.

---
+++ app/src/main/java/com/hristiyantodorov/weatherapp/views/weather_details/WeatherDetailsActivity.java
@@ -0,0 +1,51 @@
+package com.hristiyantodorov.weatherapp.views.weather_details;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.hristiyantodorov.weatherapp.R;
+
+public class WeatherDetailsActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_details);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content, WeatherDetailsFragment.newInstance());
+        fragmentTransaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/hristiyantodorov/weatherapp/views/weather_details/WeatherDetailsFragment.java
@@ -0,0 +1,36 @@
+package com.hristiyantodorov.weatherapp.views.weather_details;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hristiyantodorov.weatherapp.R;
+
+import butterknife.ButterKnife;
+
+
+public class WeatherDetailsFragment extends Fragment {
+
+
+    public WeatherDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    public static WeatherDetailsFragment newInstance() {
+        return new WeatherDetailsFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weather_details, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+}
